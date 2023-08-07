44d6afe7c3e3823b338b1b0d300899ca9aec4cfc
bmsk <vv99911@gmail.com>
2023-08-07 10:34:43 +0900

    WaterCounter: 뷰를 새로 그리지 못하는 경우
    이렇게 하면 뷰를 새로 그리지 않음

--- app/src/main/java/org/bmsk/basicstatecode/ui/watercounter/WaterCounter.kt
+++ app/src/main/java/org/bmsk/basicstatecode/ui/watercounter/WaterCounter.kt
@@ -1,6 +1,7 @@
 package org.bmsk.basicstatecode.ui.watercounter
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -8,6 +9,9 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count = 0
+    var count = 0
     Text(text = "${count}잔의 물을 마셨습니다.", modifier = modifier.padding(16.dp))
+    Button(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp)) {
+        Text(text = "한 잔 추가")
+    }
 }
