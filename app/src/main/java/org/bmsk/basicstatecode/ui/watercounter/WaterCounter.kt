fa54da53d949db4b758499e763dfcc212402b70a
bmsk <vv99911@gmail.com>
2023-08-07 10:42:45 +0900

    WaterCounter: 인라인 함수 remember 사용

--- app/src/main/java/org/bmsk/basicstatecode/ui/watercounter/WaterCounter.kt
+++ app/src/main/java/org/bmsk/basicstatecode/ui/watercounter/WaterCounter.kt
@@ -1,17 +1,25 @@
 package org.bmsk.basicstatecode.ui.watercounter
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
-    Text(text = "${count}잔의 물을 마셨습니다.", modifier = modifier.padding(16.dp))
-    Button(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp)) {
-        Text(text = "한 잔 추가")
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+
+        Text(text = "${count}잔의 물을 마셨습니다.")
+        Button(onClick = { count++ }, modifier = Modifier.padding(top = 8.dp)) {
+            Text(text = "한 잔 추가")
+        }
     }
 }
